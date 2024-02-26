7e12665c1e5c74b96aa1fd71489021fdf0c08d9a
Abere Fejiro <aberefejiro@gmail.com>
2024-02-26 19:22:41 +0100

    styled to pixel perfection and updated README

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./html"],
+  content: ["./index.html"],
   theme: {
     extend: {
       colors: {
         violet: "hsl(257, 40%, 49%)",
         "soft-magenta": "hsl(300, 69%, 71%)",
+        "hover-color": "hsl(300, 90%, 80%)",
       },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
         "open-sans": ["Open Sans", "serif"],
+      },
+      backgroundImage: {
+        "desktop-pattern": "url(/assets/images/bg-desktop.svg)",
+        "mobile-pattern": "url(/assets/images/bg-mobile.svg)",
       },
     },
   },
